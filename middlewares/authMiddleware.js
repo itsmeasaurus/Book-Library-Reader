dcb0298e105552d5ad7c0eb1b85d6739ffa6d134
Htet Wai Yan Aung <waiyan@andfun.biz>
2024-06-13 00:22:54 +0700

    Fix code errors

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
-    const header = req.header('Authorization')
-
-    if(!header) {
-        return res.status(401).json({ message: 'No token, auhorization denied'})
+    const header = req.header('Authorization');
+    if (!header) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
-    const token = header.split(' ')[1]
+    const token = header.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, 'jwtsecretkey')
-        req.user = decoded
-        next()
+        const decoded = jwt.verify(token, 'jwtsecretkey');
+        req.user = decoded;
+        next();
     } catch (error) {
-        res.status(401).json({ message: 'Token is not valid'})
+        res.status(401).json({ message: 'Token is not valid' });
     }
-}
+};
 
 module.exports = authMiddleware
--- models/User.js
+++ models/User.js
@@ -18,22 +18,22 @@ const UserSchema = new mongoose.Schema({
 })
 
 UserSchema.pre('save', async function(next) {
-    const user = this
-    if(!user.isModified('password')) return next()
-    user.password = this.encryptPassword(user.password)
-    next()
+    if(!this.isModified('password')) return next()
+    try {
+        this.password = await this.encryptPassword(this.password)
+        next()
+    } catch (error) {
+        next(error)
+    }
 })
 
-UserSchema.methods = {
-    encryptPassword: async function(password) {
-        const salt = await bcrypt.genSalt(10)
-        return await bcrypt.hash(password, salt) 
-    },
-    comparePassword: async function(password) {
-        return await bcrypt.compare(password, this.password)
-    },
+UserSchema.methods.encryptPassword = async function(password) {
+    const salt = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt) 
 }
 
-UserSchema.methods.
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)
+}
 
 module.exports = mongoose.model('User', UserSchema)
